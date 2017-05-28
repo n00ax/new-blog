e7f70898e93e8511c79167355678ec26b7ec8590
Noah Whiteis <nwhiteis@Lilacgrove-Mobile.local>
2017-05-28 04:34:22 -0500

    make it better

--- src/new_blog/backend/blog_backend.clj
+++ src/new_blog/backend/blog_backend.clj
@@ -5,18 +5,33 @@
               [monger.query :as mongo-query]
               [monger.operators :as mo]))
 
+(def connect-uri "mongodb://server-node:12345@ds155411.mlab.com:55411/heroku_vsdsxqp7")
+(def connect-database "heroku_vsdsxqp7")
+
+(defn mongo-connect-shim
+    []
+    (if (= (System/getenv "IS_PRODUCTION") "true")
+            (:db (mongo/connect-via-uri connect-uri))
+            (mongo/connect)))
+
+(defn mongo-db-shim
+    []
+    (if (= (System/getenv "IS_PRODUCTION") "true")
+        connect-database
+        "clojure-blog"))
+
 (defn grab-entry
     [id]
     "returns raw database entry as map via corrosponding id field"
-    (let [connection (mongo/connect)
-          database (mongo/get-db connection "clojure-blog")]
+    (let [connection (mongo-connect-shim)
+          database (mongo/get-db connection (mongo-db-shim))]
         (mongo-collection/find-one-as-map database "entries" {:entry-title id})))
 
 (defn grab-newest-entries
     [limit]
     "returns raw entries in ascending order created to limit as vector"
-    (let [connection (mongo/connect)
-          database (mongo/get-db connection "clojure-blog")]
+    (let [connection (mongo-connect-shim)
+          database (mongo/get-db connection (mongo-db-shim))]
         (mongo-query/with-collection database "entries"
             (mongo-query/sort (array-map :_id -1))
             (mongo-query/limit limit))))
@@ -28,11 +43,11 @@
 (defn write-comment
     [id date name comment]
     (println id date name comment)
-    (let [connection (mongo/connect)
-            database (mongo/get-db connection "clojure-blog")]
+    (let [connection (mongo-connect-shim)
+            database (mongo/get-db connection (mongo-db-shim))]
         (mongo-collection/update database "entries" {:entry-title id} {mo/$push {:entry-comments{
-                                                                                                :comment-name name
-                                                                                                :comment-date date
-                                                                                                :comment-content comment}}} {:multi false})))
+                                                                                                 :comment-name name
+                                                                                                 :comment-date date
+                                                                                                 :comment-content comment}}} {:multi false})))
         
     
